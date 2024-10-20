c6c2686a0c7123f9a4a78806f46b469be2e7a37c
Jerome <fabricantejerome@gmail.com>
2024-10-20 13:32:23 +0800

    Add deployment script

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+if !Admin.exists?(email: "admin@local.com")
+  Admin.create!(email: "admin@local.com", password: "password")
+end
